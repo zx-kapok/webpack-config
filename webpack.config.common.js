2896f04ba37f9738f4550c37b47fc86ac2ebbeaa
zx <zhaoxi@fulu.com>
2022-05-14 10:19:59 +0800

    添加

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -46,6 +46,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "a test program",
       template: "./src/index.html",
+      favicon: "./src/assets/favicon.ico"
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "server", // 可以是 server、static、json、disabled。在server模式下，分析器将启动HTTP服务器来显示软件包报告。在“静态”模式下，会生成带有报告的单个HTML文件。在disabled模式下，你可以使用这个插件来将generateStatsFile设置为true来生成Webpack Stats JSON文件。
@@ -53,7 +54,7 @@ module.exports = {
       analyzerPort: 1234, // 端口号
       reportFilename: "report.html", // 路径捆绑，将在static模式下生成的报告文件。相对于捆绑输出目录
       defaultSizes: "parsed", // 默认显示在报告中的模块大小匹配方式。应该是stat，parsed或者gzip中的一个
-      openAnalyzer: false, // 在默认浏览器中是否自动打开报告，默认 true
+      openAnalyzer: true, // 在默认浏览器中是否自动打开报告，默认 true
       generateStatsFile: false, // 如果为true，则Webpack Stats JSON文件将在bundle输出目录中生成
       statsFilename: "stats.json", // 相对于捆绑输出目录
       statsOptions: null, //stats.toJson()方法的选项。例如，您可以使用source：false选项排除统计文件中模块的来源。在这里查看更多选项：https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
